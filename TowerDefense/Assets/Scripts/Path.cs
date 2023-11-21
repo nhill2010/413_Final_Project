fbee584d7f4fe6af43776ed07caf7f322ced09a8
MichaelVertin <msv68@nau.edu>
2023-11-21 11:52:38 -0700

    Move Physical Path Backward"

--- TowerDefense/Assets/Scripts/Path.cs
+++ TowerDefense/Assets/Scripts/Path.cs
@@ -146,5 +146,7 @@ public class Path : MonoBehaviour
             outlineCircleGO.transform.localScale = outlineScale;
             backgroundCircleGO.transform.localScale = backgroundScale;
         }
+
+        this.transform.position += new Vector3(0, 0, 10);
     }
 }
